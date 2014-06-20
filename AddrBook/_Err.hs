79f589bc5572056d3a7ddcf244b007a7b69d1ed2
Grant Mather <hcaulfield57@gmail.com>
2014-06-19 20:14:55 -0600

    some new ideas

---
+++ AddrBook/_Err.hs
@@ -0,0 +1,11 @@
+module Err (err) where
+
+import System.Environment
+import System.Exit
+import System.IO
+
+err :: Int -> String -> IO ()
+err code errMsg = do
+    arg0 <- getProgName
+    hPutStrLn stderr $ arg0 ++ ": " ++ errMsg
+    exitWith (ExitFailure code)
